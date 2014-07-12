f4cd9ae1e86af9d6dac4d7b0035c09110598a2fe
Rohit Kelapure <kelapure@gmail.com>
2014-07-11 22:50:38 -0400

    First pass at test

---
+++ src/com/example/FizzBuzz.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+	
+	public static List<String> getOutput(){
+		
+		List<String> output = new ArrayList<>();
+		
+		return output;
+	}
+	
+
+}
---
+++ src/com/example/test/FizzBuzzTest.java
@@ -0,0 +1,36 @@
+package com.example.test;
+
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.hamcrest.collection.IsIterableContainingInOrder;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.example.FizzBuzz;
+
+
+public class FizzBuzzTest {
+
+	public List<String> expectedOutput;
+	
+	@Before
+	public void setup() throws IOException {
+		expectedOutput = Files
+				.lines(Paths.get("resources", "expected-output.txt"))
+				.map(s -> s.trim()).filter(s -> !s.isEmpty())
+				.collect(Collectors.toList());
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void test() {
+		assertThat(FizzBuzz.getOutput(), IsIterableContainingInOrder.contains(expectedOutput));
+	}
+
+}
